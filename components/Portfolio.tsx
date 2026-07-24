8089e9cb002af4fd48a09aedce62c555d9582b56
Aswin <aswin@example.com>
2026-07-24 21:15:59 +0530

    Add robot threat thumbnail to portfolio

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const thumbnails = [
   "/gta_thumbnail.jpg",
   "/doomsday.jpg",
+  "/robot_gun.jpg",
   "https://res.cloudinary.com/dda3aguyo/image/upload/q_auto/f_auto/v1779505023/Untitled-1_nfr2lo.jpg",
   "https://res.cloudinary.com/dda3aguyo/image/upload/q_auto/f_auto/v1779505023/fish_tank_1x_1-1_de75y5.jpg",
   "https://res.cloudinary.com/dda3aguyo/image/upload/q_auto/f_auto/v1779505024/dhurandar_2_ayush_osgwba.png",
